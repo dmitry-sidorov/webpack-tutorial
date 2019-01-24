d78ae7e049a06ed5ac338b6cbe548d4367edb6b5
Dmitry Sidorov <jimmaggot@mail.ru>
2019-01-24 17:06:56 +0300

    add: Asset Management begin

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import _ from 'lodash';
+import './style.css';
+import image from './image.jpg';
 
 function component() {
     let element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    let myImage = new Image();
+    myImage.src = image;
+    element.appendChild(myImage);
+
     return element;
 }
 
