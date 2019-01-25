1ab4e6d94e3d80442fc7bedd896fe7a469783410
Dmitry Sidorov <jimmaggot@mail.ru>
2019-01-25 16:25:39 +0300

    add: Output Management

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
 import _ from 'lodash';
-import './style.css';
-import image from './image.jpg';
-import Data from './data.xml';
 
 function component() {
     let element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    let myImage = new Image();
-    myImage.src = image;
-    element.appendChild(myImage);
-    
-    console.log(Data);
-
     return element;
 }
 
